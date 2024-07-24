3460c2ff4451f0ccf3bb7594c0edca5337711beb
syedusmanhabib <126954761+syedusmanhabib@users.noreply.github.com>
2024-07-24 10:51:38 +0000

    conversion

---
+++ maani3.js
@@ -0,0 +1,11 @@
+// let score = true
+
+// console.log(typeof score);
+
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber );
+// console.log(valueInNumber); 
+
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
